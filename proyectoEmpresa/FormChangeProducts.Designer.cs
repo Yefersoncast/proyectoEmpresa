a252dc5677b8b8f59afdc0691e91d2e49b9ca27e
Juan josé González <juan.jo.gonza70@gmail.com>
2020-06-03 20:10:59 -0500

    Primera version de la interfaz para modificar los productos haciendolo manualmente de acuerdo al id del producto que se quiera modificar nota: LAS PETICIONES NO ESTAN EN 3 CAPAS DEBIDO A QUE SE PIDEN DEL SERVIDOR, NO SE MANDAN

--- proyectoEmpresa/FormChangeProducts.Designer.cs
+++ proyectoEmpresa/FormChangeProducts.Designer.cs
@@ -31,32 +31,31 @@
             this.label1 = new System.Windows.Forms.Label();
             this.cbSelectCategory = new System.Windows.Forms.ComboBox();
             this.tbSearchProduct = new System.Windows.Forms.TextBox();
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.IdProduct = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.nameProduct = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.priceProduct = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.descriptionProduct = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.category = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.gbChanges = new System.Windows.Forms.GroupBox();
-            this.tbChangeNam = new System.Windows.Forms.TextBox();
-            this.tbChangePrice = new System.Windows.Forms.TextBox();
-            this.tbChangeDes = new System.Windows.Forms.TextBox();
-            this.tbChangeCate = new System.Windows.Forms.TextBox();
+            this.btSave = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.btSave = new System.Windows.Forms.Button();
+            this.tbChangeCate = new System.Windows.Forms.TextBox();
+            this.tbChangeDes = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbChangePrice = new System.Windows.Forms.TextBox();
+            this.tbChangeNam = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tbFindId = new System.Windows.Forms.TextBox();
             this.btSendId = new System.Windows.Forms.Button();
+            this.btActivateCat = new System.Windows.Forms.Button();
+            this.dgvProducts = new System.Windows.Forms.DataGridView();
+            this.btShowProducts = new System.Windows.Forms.Button();
+            this.btSearchProduct = new System.Windows.Forms.Button();
             this.gbChanges.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(84, 74);
+            this.label1.Location = new System.Drawing.Point(111, 44);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(57, 13);
             this.label1.TabIndex = 0;
@@ -65,60 +64,18 @@
             // cbSelectCategory
             // 
             this.cbSelectCategory.FormattingEnabled = true;
-            this.cbSelectCategory.Location = new System.Drawing.Point(73, 144);
+            this.cbSelectCategory.Location = new System.Drawing.Point(90, 146);
             this.cbSelectCategory.Name = "cbSelectCategory";
             this.cbSelectCategory.Size = new System.Drawing.Size(114, 21);
             this.cbSelectCategory.TabIndex = 1;
-            this.cbSelectCategory.Text = "Categorias";
-            this.cbSelectCategory.SelectedIndexChanged += new System.EventHandler(this.cbSelectCategory_SelectedIndexChanged);
             // 
             // tbSearchProduct
             // 
-            this.tbSearchProduct.Location = new System.Drawing.Point(73, 118);
+            this.tbSearchProduct.Location = new System.Drawing.Point(88, 78);
             this.tbSearchProduct.Name = "tbSearchProduct";
             this.tbSearchProduct.Size = new System.Drawing.Size(100, 20);
             this.tbSearchProduct.TabIndex = 2;
             this.tbSearchProduct.Text = "Buscar Producto";
-            // 
-            // listView1
-            // 
-            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.IdProduct,
-            this.nameProduct,
-            this.descriptionProduct,
-            this.priceProduct,
-            this.category});
-            this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(314, 4);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(474, 408);
-            this.listView1.TabIndex = 3;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.Details;
-            // 
-            // IdProduct
-            // 
-            this.IdProduct.Text = "ID";
-            // 
-            // nameProduct
-            // 
-            this.nameProduct.Text = "Nombre";
-            // 
-            // priceProduct
-            // 
-            this.priceProduct.DisplayIndex = 2;
-            this.priceProduct.Text = "Precio";
-            // 
-            // descriptionProduct
-            // 
-            this.descriptionProduct.DisplayIndex = 3;
-            this.descriptionProduct.Text = "Descripcion";
-            this.descriptionProduct.Width = 180;
-            // 
-            // category
-            // 
-            this.category.Text = "Categoria";
-            this.category.Width = 106;
             // 
             // gbChanges
             // 
@@ -131,41 +88,22 @@
             this.gbChanges.Controls.Add(this.label4);
             this.gbChanges.Controls.Add(this.tbChangePrice);
             this.gbChanges.Controls.Add(this.tbChangeNam);
-            this.gbChanges.Location = new System.Drawing.Point(12, 227);
+            this.gbChanges.Location = new System.Drawing.Point(12, 260);
             this.gbChanges.Name = "gbChanges";
-            this.gbChanges.Size = new System.Drawing.Size(277, 185);
+            this.gbChanges.Size = new System.Drawing.Size(277, 188);
             this.gbChanges.TabIndex = 4;
             this.gbChanges.TabStop = false;
             this.gbChanges.Text = "Cambios";
             this.gbChanges.Visible = false;
             // 
-            // tbChangeNam
+            // btSave
             // 
-            this.tbChangeNam.Location = new System.Drawing.Point(76, 34);
-            this.tbChangeNam.Name = "tbChangeNam";
-            this.tbChangeNam.Size = new System.Drawing.Size(100, 20);
-            this.tbChangeNam.TabIndex = 0;
-            // 
-            // tbChangePrice
-            // 
-            this.tbChangePrice.Location = new System.Drawing.Point(76, 61);
-            this.tbChangePrice.Name = "tbChangePrice";
-            this.tbChangePrice.Size = new System.Drawing.Size(100, 20);
-            this.tbChangePrice.TabIndex = 1;
-            // 
-            // tbChangeDes
-            // 
-            this.tbChangeDes.Location = new System.Drawing.Point(76, 88);
-            this.tbChangeDes.Name = "tbChangeDes";
-            this.tbChangeDes.Size = new System.Drawing.Size(100, 20);
-            this.tbChangeDes.TabIndex = 2;
-            // 
-            // tbChangeCate
-            // 
-            this.tbChangeCate.Location = new System.Drawing.Point(76, 115);
-            this.tbChangeCate.Name = "tbChangeCate";
-            this.tbChangeCate.Size = new System.Drawing.Size(187, 20);
-            this.tbChangeCate.TabIndex = 3;
+            this.btSave.Location = new System.Drawing.Point(188, 156);
+            this.btSave.Name = "btSave";
+            this.btSave.Size = new System.Drawing.Size(75, 23);
+            this.btSave.TabIndex = 10;
+            this.btSave.Text = "Guardar";
+            this.btSave.UseVisualStyleBackColor = true;
             // 
             // label3
             // 
@@ -175,15 +113,6 @@
             this.label3.Size = new System.Drawing.Size(47, 13);
             this.label3.TabIndex = 6;
             this.label3.Text = "Nombre:";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(6, 64);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(40, 13);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "Precio:";
             // 
             // label5
             // 
@@ -203,50 +132,119 @@
             this.label6.TabIndex = 9;
             this.label6.Text = "Descripcion:";
             // 
-            // btSave
+            // tbChangeCate
             // 
-            this.btSave.Location = new System.Drawing.Point(188, 156);
-            this.btSave.Name = "btSave";
-            this.btSave.Size = new System.Drawing.Size(75, 23);
-            this.btSave.TabIndex = 10;
-            this.btSave.Text = "Guardar";
-            this.btSave.UseVisualStyleBackColor = true;
+            this.tbChangeCate.Location = new System.Drawing.Point(76, 115);
+            this.tbChangeCate.Name = "tbChangeCate";
+            this.tbChangeCate.Size = new System.Drawing.Size(187, 20);
+            this.tbChangeCate.TabIndex = 3;
+            // 
+            // tbChangeDes
+            // 
+            this.tbChangeDes.Location = new System.Drawing.Point(76, 88);
+            this.tbChangeDes.Name = "tbChangeDes";
+            this.tbChangeDes.Size = new System.Drawing.Size(100, 20);
+            this.tbChangeDes.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Precio:";
+            // 
+            // tbChangePrice
+            // 
+            this.tbChangePrice.Location = new System.Drawing.Point(76, 61);
+            this.tbChangePrice.Name = "tbChangePrice";
+            this.tbChangePrice.Size = new System.Drawing.Size(100, 20);
+            this.tbChangePrice.TabIndex = 1;
+            // 
+            // tbChangeNam
+            // 
+            this.tbChangeNam.Location = new System.Drawing.Point(76, 34);
+            this.tbChangeNam.Name = "tbChangeNam";
+            this.tbChangeNam.Size = new System.Drawing.Size(100, 20);
+            this.tbChangeNam.TabIndex = 0;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 198);
+            this.label2.Location = new System.Drawing.Point(15, 236);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(50, 13);
             this.label2.TabIndex = 5;
             this.label2.Text = "Modificar";
             // 
-            // textBox1
+            // tbFindId
             // 
-            this.textBox1.Location = new System.Drawing.Point(69, 198);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 6;
+            this.tbFindId.Location = new System.Drawing.Point(68, 233);
+            this.tbFindId.Name = "tbFindId";
+            this.tbFindId.Size = new System.Drawing.Size(100, 20);
+            this.tbFindId.TabIndex = 6;
             // 
             // btSendId
             // 
-            this.btSendId.Location = new System.Drawing.Point(176, 194);
+            this.btSendId.Location = new System.Drawing.Point(174, 231);
             this.btSendId.Name = "btSendId";
             this.btSendId.Size = new System.Drawing.Size(75, 23);
             this.btSendId.TabIndex = 7;
             this.btSendId.Text = "Enviar";
             this.btSendId.UseVisualStyleBackColor = true;
             // 
+            // btActivateCat
+            // 
+            this.btActivateCat.Location = new System.Drawing.Point(9, 144);
+            this.btActivateCat.Name = "btActivateCat";
+            this.btActivateCat.Size = new System.Drawing.Size(75, 23);
+            this.btActivateCat.TabIndex = 8;
+            this.btActivateCat.Text = "Ver";
+            this.btActivateCat.UseVisualStyleBackColor = true;
+            this.btActivateCat.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dgvProducts
+            // 
+            this.dgvProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProducts.Location = new System.Drawing.Point(281, 44);
+            this.dgvProducts.Name = "dgvProducts";
+            this.dgvProducts.Size = new System.Drawing.Size(507, 368);
+            this.dgvProducts.TabIndex = 9;
+            // 
+            // btShowProducts
+            // 
+            this.btShowProducts.Location = new System.Drawing.Point(88, 174);
+            this.btShowProducts.Name = "btShowProducts";
+            this.btShowProducts.Size = new System.Drawing.Size(116, 23);
+            this.btShowProducts.TabIndex = 10;
+            this.btShowProducts.Text = "Mostrar";
+            this.btShowProducts.UseVisualStyleBackColor = true;
+            this.btShowProducts.Click += new System.EventHandler(this.btShowProducts_Click);
+            // 
+            // btSearchProduct
+            // 
+            this.btSearchProduct.Location = new System.Drawing.Point(101, 104);
+            this.btSearchProduct.Name = "btSearchProduct";
+            this.btSearchProduct.Size = new System.Drawing.Size(75, 23);
+            this.btSearchProduct.TabIndex = 11;
+            this.btSearchProduct.Text = "Buscar";
+            this.btSearchProduct.UseVisualStyleBackColor = true;
+            this.btSearchProduct.Click += new System.EventHandler(this.btSearchProduct_Click);
+            // 
             // FormChangeProducts
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btSearchProduct);
+            this.Controls.Add(this.btShowProducts);
+            this.Controls.Add(this.dgvProducts);
+            this.Controls.Add(this.btActivateCat);
             this.Controls.Add(this.btSendId);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tbFindId);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.gbChanges);
-            this.Controls.Add(this.listView1);
             this.Controls.Add(this.tbSearchProduct);
             this.Controls.Add(this.cbSelectCategory);
             this.Controls.Add(this.label1);
@@ -254,6 +252,7 @@
             this.Text = "FormChangeProducts";
             this.gbChanges.ResumeLayout(false);
             this.gbChanges.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -264,12 +263,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox cbSelectCategory;
         private System.Windows.Forms.TextBox tbSearchProduct;
-        private System.Windows.Forms.ListView listView1;
-        private System.Windows.Forms.ColumnHeader IdProduct;
-        private System.Windows.Forms.ColumnHeader nameProduct;
-        private System.Windows.Forms.ColumnHeader descriptionProduct;
-        private System.Windows.Forms.ColumnHeader priceProduct;
-        private System.Windows.Forms.ColumnHeader category;
         private System.Windows.Forms.GroupBox gbChanges;
         private System.Windows.Forms.Button btSave;
         private System.Windows.Forms.Label label3;
@@ -281,7 +274,11 @@
         private System.Windows.Forms.TextBox tbChangePrice;
         private System.Windows.Forms.TextBox tbChangeNam;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tbFindId;
         private System.Windows.Forms.Button btSendId;
+        private System.Windows.Forms.Button btActivateCat;
+        private System.Windows.Forms.DataGridView dgvProducts;
+        private System.Windows.Forms.Button btShowProducts;
+        private System.Windows.Forms.Button btSearchProduct;
     }
 }
